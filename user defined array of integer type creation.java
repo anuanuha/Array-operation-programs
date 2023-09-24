0719591c4bb1d923ac933d670e0fe2da59caabcb
ANU. H A <125720369+anuanuha@users.noreply.github.com>
2023-09-24 22:09:04 +0530

    user defined array of integer type creation

---
+++ user defined array of integer type creation.java
@@ -0,0 +1,24 @@
+package array;
+
+import java.util.Scanner;
+
+public class dynamic_array {
+	    public static void main(String[] args) {
+	      Scanner sc=new Scanner(System.in);
+	      System.out.println("enter the array");
+	      int size=sc.nextInt()	;
+	      int[]arr = new int[size];
+	      System.out.println("enter the array values");
+	      for(int i=0;i<arr.length;i++)
+	      {
+	    	   arr[i]=sc.nextInt();
+	    	  //System.out.print(arr[i]=sc.nextInt());
+	      }
+	      System.out.println("the array values are:");
+	      for(int i=0;i<arr.length;i++)
+	      {
+	    	  System.out.print(arr[i]+" ");
+	      }
+
+	    }
+}
