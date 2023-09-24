53061d0984449d30f8280e480d7664df6896cfe7
ANU. H A <125720369+anuanuha@users.noreply.github.com>
2023-09-24 22:07:25 +0530

    user defined array of string type creation

---
+++ user defined array of string  type creation.java
@@ -0,0 +1,24 @@
+package array;
+
+import java.util.Scanner;
+
+public class dynamic_array_string {
+	public static void main(String[] args) {
+	      Scanner sc=new Scanner(System.in);
+	      System.out.println("enter the array");
+	      int size=sc.nextInt()	;
+	      String[] arr = new String[size];
+	      System.out.println("enter the array values");
+	      for(int i=0;i<arr.length;i++)
+	      {
+	    	   arr[i]=sc.next();
+	    	  //System.out.print(arr[i]=sc.nextInt());
+	      }
+	      System.out.println("the array values are:");
+	      for(int i=0;i<arr.length;i++)
+	      {
+	    	  System.out.print(arr[i]+" ");
+	      }
+
+	    }
+}
