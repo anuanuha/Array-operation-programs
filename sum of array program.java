b64a9e79686caada77cfa0ac93ef34beae30f4e3
ANU. H A <125720369+anuanuha@users.noreply.github.com>
2023-09-24 22:01:11 +0530

    sum of array program

---
+++ sum of array program.java
@@ -0,0 +1,12 @@
+package aaaa;
+
+public class sumarray {
+public static void main(String[] args) {
+	int sum=0;
+	int[] arr= {1,2,3,4,5,6,7};
+	for (int i = 0; i < arr.length; i++) {
+		sum=sum+arr[i];			
+	}
+	System.out.println(sum);
+}
+}
